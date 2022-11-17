39754aa85ebb9a9e2054f790cf781fea16aa0613
orelsprojects <orelsmail@gmail.com>
2022-11-17 10:17:22 +0200

    Add new json for test

--- components/content.jsx
+++ components/content.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import mobileLottie from '../public/lottie_mobile_development';
-import fullStackLottie from '../public/web-dev';
+import fullStackLottie from '../public/test.json';
 import DetailsCard from './details-card/details-card';
 
 const mobileLottieConfig = {
