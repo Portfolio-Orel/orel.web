ca8f0ae8bf20c63ed9361a7373ce9051ee755967
orelsprojects <orelsmail@gmail.com>
2022-11-20 11:42:41 +0200

    improved about description

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,11 +1,19 @@
+import Link from 'next/link';
 import React from 'react';
 import data from '../../asset/data.json';
-import Link from 'next/link';
 
 export default function About() {
   return (
     <div className="h-screen w-screen flex flex-col items-center justify-center">
-      <h1 className="text-text">{data.about}</h1>
+      {data.about.paragraphs
+        ? data.about.paragraphs.map((paragraph) => {
+            return (
+              <>
+                {Paragraph(paragraph)} <br />
+              </>
+            );
+          })
+        : 'Hello'}
       <Link className="text-text" href="files/cover-letter.pdf" target="_blank" rel="noreferrer">
         Cover Letter
       </Link>
@@ -15,3 +23,18 @@ export default function About() {
     </div>
   );
 }
+
+const Paragraph = (paragraph) => {
+  const buildPragraph = () => {
+    const { lines } = paragraph;
+    return lines.map((line, index) => {
+      return (
+        <span key={index} className="text-text text-lg">
+          {line}
+        </span>
+      );
+    });
+  };
+
+  return <body className="text-text text-lg font-light">{buildPragraph()}</body>;
+};
