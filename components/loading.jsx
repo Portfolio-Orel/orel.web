f8e90719fc2403d5ea24a88808980946fd072d60
orelsprojects <orelsmail@gmail.com>
2022-11-29 11:23:43 +0200

    Fix loading

---
+++ components/loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="animate-pulse rounded-full w-8 h-8 bg-text"/>
+    </div>
+  );
+}
