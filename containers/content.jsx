ddfaeae5831f022543265878c1ba993b2b6b5baf
orelsprojects <orelsmail@gmail.com>
2023-02-04 18:50:33 +0200

    Test game

--- containers/content.jsx
+++ containers/content.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import Router from 'next/router';
 import mobileLottie from '../public/lottie_mobile_development.json';
 import fullStackLottie from '../public/lottie_web_development.json';
 import certificateLottie from '../public/lottie_certificate.json';
 import DetailsCard from '../components/details-card';
-import { useSelector } from 'react-redux';
-import Router from 'next/router';
+import Button from '../components/button';
 
 const mobileLottieConfig = {
   loop: true,
@@ -37,12 +37,14 @@ const certificateLottieConfig = {
 const TOPIC_FULL_STACK = 'fullstack';
 const TOPIC_MOBILE = 'mobile';
 const TOPIC_CERTIFICATIONS = 'certifications';
+const TOPIC_GAME = 'game';
 
 export default function Content() {
   const [selectedProfession, setSelectedProfession] = useState('');
-  const user = useSelector((state) => state?.user);
 
-  const setNewRoute = () => Router.push(`/${selectedProfession}`);
+  function setNewRoute() {
+    Router.push(`/${selectedProfession}`);
+  }
 
   useEffect(() => {
     if (selectedProfession) {
@@ -55,45 +57,52 @@ export default function Content() {
   }, []);
 
   return (
-    <div className="h-full w-full flex flex-row justify-center items-center">
-      <DetailsCard
-        title="Full Stack"
-        experience="4"
-        className={selectedProfession != '' ? 'z-0' : 'z-20'}
-        lottieConfig={fullStackLottieConfig}
-        onSelected={() => {
-          setSelectedProfession(TOPIC_FULL_STACK);
-        }}
-        onSelectedDismiss={() => {
-          setSelectedProfession('');
-        }}
-        visible={selectedProfession == '' || selectedProfession == TOPIC_FULL_STACK}
-      />
-      <DetailsCard
-        title="Mobile"
-        experience="4"
-        className={selectedProfession != '' ? 'z-0' : 'z-20'}
-        lottieConfig={mobileLottieConfig}
-        onSelected={() => {
-          setSelectedProfession(TOPIC_MOBILE);
-        }}
-        onSelectedDismiss={() => {
-          setSelectedProfession('');
-        }}
-        visible={selectedProfession == '' || selectedProfession == TOPIC_MOBILE}
-      />
-      <DetailsCard
-        title="Certificates"
-        className={selectedProfession != '' ? 'z-0' : 'z-20'}
-        lottieConfig={certificateLottieConfig}
-        onSelected={() => {
-          setSelectedProfession(TOPIC_CERTIFICATIONS);
-        }}
-        onSelectedDismiss={() => {
-          setSelectedProfession('');
-        }}
-        visible={selectedProfession == '' || selectedProfession == TOPIC_CERTIFICATIONS}
-      />
+    <div className="h-full w-full flex-column justify-center items-center">
+      <div className="h-full w-full flex flex-row justify-center items-center">
+        <DetailsCard
+          title="Full Stack"
+          experience="4"
+          className={selectedProfession !== '' ? 'z-0' : 'z-20'}
+          lottieConfig={fullStackLottieConfig}
+          onSelected={() => {
+            setSelectedProfession(TOPIC_FULL_STACK);
+          }}
+          onSelectedDismiss={() => {
+            setSelectedProfession('');
+          }}
+          visible={selectedProfession === '' || selectedProfession === TOPIC_FULL_STACK}
+        />
+        <DetailsCard
+          title="Mobile"
+          experience="4"
+          className={selectedProfession !== '' ? 'z-0' : 'z-20'}
+          lottieConfig={mobileLottieConfig}
+          onSelected={() => {
+            setSelectedProfession(TOPIC_MOBILE);
+          }}
+          onSelectedDismiss={() => {
+            setSelectedProfession('');
+          }}
+          visible={selectedProfession === '' || selectedProfession === TOPIC_MOBILE}
+        />
+        <DetailsCard
+          title="Certificates"
+          className={selectedProfession !== '' ? 'z-0' : 'z-20'}
+          lottieConfig={certificateLottieConfig}
+          onSelected={() => {
+            setSelectedProfession(TOPIC_CERTIFICATIONS);
+          }}
+          onSelectedDismiss={() => {
+            setSelectedProfession('');
+          }}
+          visible={selectedProfession === '' || selectedProfession === TOPIC_CERTIFICATIONS}
+        />
+        <Button
+          onClick={() => setSelectedProfession(TOPIC_GAME)}
+          className="z-20"
+          text="Or play it!"
+        />
+      </div>
     </div>
   );
 }
---
+++ pages/game.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useRef } from 'react';
+import { Unity, useUnityContext } from 'react-unity-webgl';
+import Router from 'next/router';
+
+export default function Game() {
+  const iframeRef = useRef();
+  const unityFrameRef = useRef();
+
+  const { unityProvider } = useUnityContext({
+    loaderUrl: '/unity/Build/Build.loader.js',
+    dataUrl: '/unity/Build/Build.data',
+    frameworkUrl: '/unity/Build/Build.framework.js',
+    codeUrl: '/unity/Build/Build.wasm',
+  });
+
+  // on router change event, clear iframe
+  useEffect(() => {
+    const handleRouteChange = () => {
+      if (iframeRef !== null) {
+        iframeRef.current.removeChild(iframeRef.current.children[0]);
+      }
+    };
+    Router.events.on('routeChangeStart', handleRouteChange);
+    return () => {
+      Router.events.off('routeChangeStart', handleRouteChange);
+    };
+  }, []);
+
+  return (
+    <div className="h-full w-full m-0" id="iframeContainer" ref={iframeRef}>
+      <Unity className="h-full w-full" unityProvider={unityProvider} ref={unityFrameRef} />
+    </div>
+  );
+}
