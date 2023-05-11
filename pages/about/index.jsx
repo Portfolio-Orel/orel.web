1b32ea96b5d0bcddd15725303e46cf2c3d35bfb8
Orelzman <orelsmail@gmail.com>
2023-05-11 14:30:10 +0300

    - Update cv + cover-letter

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -19,21 +19,21 @@ function Paragraph(paragraph) {
 export default function About() {
   return (
     <div className="h-full w-full flex flex-col justify-center items-center">
-      <div className='flex flex-col justify-center align-center mx-96'>
-      {data.about.paragraphs
-        ? data.about.paragraphs.map((paragraph) => (
-            <>
-              {Paragraph(paragraph)} <br />
-            </>
-          ))
-        : ''}
-        </div>
+      <div className="flex flex-col justify-center align-center mx-96">
+        {data.about.paragraphs
+          ? data.about.paragraphs.map((paragraph) => (
+              <>
+                {Paragraph(paragraph)} <br />
+              </>
+            ))
+          : ''}
+      </div>
       <div className="flex flex-row gap-6">
         <Link className="text-text" href="files/cover-letter.pdf" target="_blank" rel="noreferrer">
           <Button text="Cover Letter" type="secondary" />
         </Link>
-        <Link href="files/resume.pdf" className="text-text" target="_blank" rel="noreferrer">
-          <Button text="Resume" type="secondary" />
+        <Link href="files/cv.pdf" className="text-text" target="_blank" rel="noreferrer">
+          <Button text="CV" type="secondary" />
         </Link>
       </div>
     </div>
